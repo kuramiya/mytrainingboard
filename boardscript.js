a96550747e69ee14555f5fb5eff304818a5c46ea
h07361 <hiroyuki.seki@horiba.com>
2020-04-28 11:28:43 +0900

    ホールド配置、名称を修正した タイマー処理のバグを修正した

--- boardscript.js
+++ boardscript.js
@@ -3,7 +3,7 @@ var holds = {};
 
 Init();
 
-Start();
+window.onload = Start();
 
 function Init()
 {
@@ -14,11 +14,12 @@ function Init()
         height: 600
     });
 
+    var boardColor = "tan";
     var leftBarX = 100;
-    var leftMainBoardX = 140;
-    var rightBarX = 400;
-    var rightMainBoardX = 320;
-    var mainHoldWidth = 60;
+    var leftMainBoardX = 160;
+    var rightBarX = 380;
+    var rightMainBoardX = 300;
+    var mainHoldWidth = 40;
 
     // then create layer
     var leftBar = new Konva.Rect({
@@ -26,7 +27,7 @@ function Init()
         y: 0,
         width: 20,
         height: 580,
-        fill: "gray",
+        fill: boardColor,
         stroke: "black",
         strokeWidth: 1,
     });
@@ -37,7 +38,7 @@ function Init()
         y: 0,
         width: 20,
         height: 580,
-        fill: "gray",
+        fill: boardColor,
         stroke: "black",
         strokeWidth: 1,
     });
@@ -46,45 +47,49 @@ function Init()
     var mainBoard = new Konva.Rect({
         x: leftBarX,
         y: 140,
-        width: 320,
+        width: 300,
         height: 120,
-        fill: "gray",
+        fill: boardColor,
         stroke: "black",
         strokeWidth: 1,
     });
     boardLayer.add(mainBoard);
 
-    holds["L1"] = CreateHold(leftBarX, 20);
-    holds["L2"] = CreateHold(leftBarX, 60,);
-    holds["L3"] = CreateHold(leftBarX - 20, 100,);
-    holds["L4"] = CreateHold(leftBarX, 140,);
-    holds["L5"] = CreateHold(leftBarX - 20, 240,);
-    holds["L6"] = CreateHold(leftBarX, 280,);
-    holds["L7"] = CreateHold(leftBarX, 420,);
-    holds["L8"] = CreateHold(leftBarX + 20, 420,);
-    holds["L9"] = CreateHold(leftBarX, 540,);
-    holds["L10"] = CreateHold(leftBarX + 20, 540,);
+    holds["LeftHand1"] = CreateHold(leftBarX, 20);
+    holds["LeftHand2"] = CreateHold(leftBarX, 60,);
+    holds["LeftHand3"] = CreateHold(leftBarX - 20, 100);
+    holds["LeftHand4"] = CreateHold(leftBarX, 140,);
+    holds["LeftHand5"] = CreateHold(leftBarX - 20, 240);
+    holds["LeftHand6"] = CreateHold(leftBarX, 280);
 
-    holds["LGaba"] = CreateHold(leftMainBoardX, 140, mainHoldWidth);
-    holds["L38mm"] = CreateHold(leftMainBoardX, 180, mainHoldWidth);
-    holds["L19mm"] = CreateHold(leftMainBoardX, 220, mainHoldWidth);
-    holds["LClip"] = CreateHold(leftBarX, 260, 20, 20, "orange");
+    holds["LeftFoot1"] = CreateHold(leftBarX, 420, 20, 20, "green");
+    holds["LeftFoot2"] = CreateHold(leftBarX + 20, 420, 20, 20, "green");
+    holds["LeftFoot3"] = CreateHold(leftBarX, 540, 20, 20, "green");
+    holds["LeftFoot4"] = CreateHold(leftBarX + 20, 540, 20, 20, "green");
 
-    holds["R1"] = CreateHold(rightBarX, 20);
-    holds["R2"] = CreateHold(rightBarX, 60);
-    holds["R3"] = CreateHold(rightBarX + 20, 100);
-    holds["R4"] = CreateHold(rightBarX, 140);
-    holds["R5"] = CreateHold(rightBarX + 20, 240);
-    holds["R6"] = CreateHold(rightBarX, 280);
-    holds["R7"] = CreateHold(rightBarX, 420);
-    holds["R8"] = CreateHold(rightBarX - 20, 420);
-    holds["R9"] = CreateHold(rightBarX, 540);
-    holds["R10"] = CreateHold(rightBarX - 20, 540);
+    holds["LeftHandGaba1"] = CreateHold(leftMainBoardX - 40, 140, mainHoldWidth);
+    holds["LeftHandGaba2"] = CreateHold(leftMainBoardX + 40, 140, mainHoldWidth);
+    holds["LeftHand38mm"] = CreateHold(leftMainBoardX, 180, mainHoldWidth);
+    holds["LeftHand19mm"] = CreateHold(leftMainBoardX, 220, mainHoldWidth);
+    holds["LeftHandClip"] = CreateHold(leftBarX, 260, 20, 20, "silver");
 
-    holds["RGaba"] = CreateHold(rightMainBoardX, 140, mainHoldWidth);
-    holds["R38mm"] = CreateHold(rightMainBoardX, 180, mainHoldWidth);
-    holds["R19mm"] = CreateHold(rightMainBoardX, 220, mainHoldWidth);
-    holds["RClip"] = CreateHold(rightBarX, 260, 20, 20, "orange");
+    holds["RightHand1"] = CreateHold(rightBarX, 20);
+    holds["RightHand2"] = CreateHold(rightBarX, 60);
+    holds["RightHand3"] = CreateHold(rightBarX + 20, 100);
+    holds["RightHand4"] = CreateHold(rightBarX, 140);
+    holds["RightHand5"] = CreateHold(rightBarX + 20, 240);
+    holds["RightHand6"] = CreateHold(rightBarX, 280);
+
+    holds["RightFoot1"] = CreateHold(rightBarX, 420, 20, 20, "green");
+    holds["RightFoot2"] = CreateHold(rightBarX - 20, 420, 20, 20, "green");
+    holds["RightFoot3"] = CreateHold(rightBarX, 540, 20, 20, "green");
+    holds["RightFoot4"] = CreateHold(rightBarX - 20, 540, 20, 20, "green");
+
+    holds["RightHandGaba1"] = CreateHold(rightMainBoardX + 40, 140, mainHoldWidth);
+    holds["RightHandGaba2"] = CreateHold(rightMainBoardX - 40, 140, mainHoldWidth);
+    holds["RightHand38mm"] = CreateHold(rightMainBoardX, 180, mainHoldWidth);
+    holds["RightHand19mm"] = CreateHold(rightMainBoardX, 220, mainHoldWidth);
+    holds["RightHandClip"] = CreateHold(rightBarX, 260, 20, 20, "silver");
 
     for(let key in holds)
     {
@@ -98,8 +103,6 @@ function Init()
         fill: "white",
     });
     boardLayer.add(str);
-
-    var ui = parseInt(document.forms.updateIntervalText.value, 10);
 
     // add the layer to the stage
     stage.add(boardLayer);
@@ -126,10 +129,10 @@ function CreateHold(x, y, width = 20, height = 20, fill = "red")
 function Start()
 {
     setInterval(function timerHandler(){
-        var hold = holds["RGaba"];
+        var hold = holds["RightHandGaba1"];
         var x = hold.x();
         hold.x(x + 1);
         boardLayer.draw();
-    }, parseInt(getElementById["updateIntervalText"].value, 10));
+    }, parseInt(document.getElementById("updateIntervalText").value, 10));
 }
 
